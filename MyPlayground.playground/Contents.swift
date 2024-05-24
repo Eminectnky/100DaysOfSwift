5888407bb9186d8edc77f773d81928a53a010f72
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-05-24 17:41:03 +0300

    Day 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+
+//Variables
+
+var greeting = "Hello, playground"
+greeting = "Goodbye"
+greeting = "Hello"
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+
+//Strings and integers
+
+var str = "Hello, playground"
+var age = 38
+var population = 8_000_000
+
+var meaningOfLife = 42
+
+
+//Multi-line strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var quote = "Change the world by being yourself"
+var  burns = """
+The best laid schemes
+O'mice and men
+Gang aft agley
+"""
+
+//Doubles and booleans
+
+var pi = 3.141
+var awesome = true
+
+
+var myInt = 1
+var myDouble = 1.0
+
+
+//String interpolation
+
+var score = 85
+var str2 = "Your score was \(score)"
+var results = "The test results are here: \(str2)"
+var city = "Cardiff"
+var message = "Welcome to \(city)!"
+
+//Constants
+
+let taylor = "swift"
+
+
+//Type annotations
+
+let str3 = "Hello, playground"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+var percentage: Double = 99
+
+var name: String
+
+
+
